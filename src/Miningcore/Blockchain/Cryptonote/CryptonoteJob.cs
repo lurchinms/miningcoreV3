d96697656d16ab5ff57f876d1c794856cc6ff2c8
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2026-05-16 08:33:55 -0500

    Refactor block hash computation for CryptonoteJob

--- src/Miningcore/Blockchain/Cryptonote/CryptonoteJob.cs
+++ src/Miningcore/Blockchain/Cryptonote/CryptonoteJob.cs
@@ -211,7 +211,7 @@ public class CryptonoteJob
             Span<byte> blockHash = stackalloc byte[32];
             
             // Not all Cryptonote coins are equal
-            if(blobType == ZephyrConstants.BlobType || blobType == ScalaConstants.ScalaBlobType)
+            if(blobType == ZephyrConstants.BlobType)
                 CryptonoteBindings.GetBlockId(blob, blockHash, blobType);
             else
                 ComputeBlockHash(blobConverted, blockHash);
